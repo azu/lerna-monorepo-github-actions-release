44169445fa9af71f3a6bd174aeca21f6714173d2
YordanLV <yordan.lasonov@gmail.com>
2021-09-14 16:06:05 +0300

    chore(release): publish 2.1.11

---
+++ packages/ui/dist/Button/Button.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function Button() {
+  return /*#__PURE__*/_react["default"].createElement("button", {
+    type: "button"
+  }, "Click Me!");
+}
+
+var _default = Button;
+exports["default"] = _default;
